d57bf6f28b546b0a97fe24cc5fef55dab1550545
elgmouri abderrahim <ea.officialbox@gmail.com>
2026-01-18 16:08:15 +0100

    create database schema

---
+++ app/Views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home Page!')
+
+@section('content')
+    <h2>Welcome, {{ $name }}</h2>
+@endsection
---
+++ app/Views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header><h1>navbar</h1></header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
